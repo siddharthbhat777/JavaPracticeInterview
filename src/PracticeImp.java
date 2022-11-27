4fbab8a7e42027ad3068a28b814cab5105f4e889
Siddharth Bhat <dbgt4531@gmail.com>
2022-11-28 02:32:44 +0530

    added vowels and add by reference

--- src/PracticeImp.java
+++ src/PracticeImp.java
@@ -3,15 +3,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PracticeImp {
+    int swapNum1 = 12, swapNum2 = 45; //need to initialize as instance variable to use call by reference
     public static void main(String[] args) {
         /* Do not remove this */
         String s = "abcba";
         int num = 153;
+        String vowelString = "Hello this is sid here.";
         System.out.println(palindrome(s)? "The string \"" + s + "\" is Palindrome" : "The string \"" + s + "\" is not Palindrome");
         System.out.println(noDuplicate(s));
         System.out.println(fibonacci(num));
         System.out.println(primeNumbers(num));
         System.out.println(armstrongNum(num)? "The number " + num + " is armstrong." : "The number " + num + " is not armstrong.");
+        // using call by reference
+        PracticeImp pi = new PracticeImp();
+        System.out.println("Numbers before swap: " + pi.swapNum1 + " " + pi.swapNum2);
+        swapWithoutTemp(pi);
+        System.out.println("Numbers after swap: " + pi.swapNum1 + " " + pi.swapNum2);
+        System.out.println(noVowelsString(vowelString));
+    }
+
+    private static String noVowelsString(String vowelString) {
+        char [] vowels = {'a', 'e', 'i', 'o', 'u'};
+        StringBuilder sb = new StringBuilder();
+        boolean isVowel = false;
+        for (int i=0; i<vowelString.length(); i++) {
+            for (char vowel : vowels) {
+                if (vowelString.charAt(i) == vowel) {
+                    isVowel = true;
+                    break;
+                } else {
+                    isVowel = false;
+                }
+            }
+            if (!isVowel) {
+                sb.append(vowelString.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    private static void swapWithoutTemp(PracticeImp pi) {
+        pi.swapNum1 = pi.swapNum1 + pi.swapNum2;
+        pi.swapNum2 = pi.swapNum1 - pi.swapNum2;
+        pi.swapNum1 = pi.swapNum1 - pi.swapNum2;
     }
 
     private static boolean armstrongNum(int num) {
