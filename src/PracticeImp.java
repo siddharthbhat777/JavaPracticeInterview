f2edf61537fa8d6450eeb23a1ebd26c9a16da485
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-18 19:54:10 +0530

    added UniqueCategories logic

--- src/PracticeImp.java
+++ src/PracticeImp.java
@@ -55,6 +55,23 @@ public class PracticeImp {
             result = arr[arrLen - 1];
         }
         return result;
+
+        /*
+        import java.util.*;
+        public class SecondLargestInArrayExample2{
+        public static int getSecondLargest(Integer[] a, int total){
+        List<Integer> list=Arrays.asList(a);
+        Collections.sort(list);
+        int element=list.get(total-2);
+        return element;
+        }
+        public static void main(String args[]){
+        Integer a[]={1,2,5,6,3,2};
+        Integer b[]={44,66,99,99,33,22,55};
+        System.out.println("Second Largest:"+getSecondLargest(a,6));
+        System.out.println("Second Largest:"+getSecondLargest(b,7));
+        }}
+        */
     }
 
     private static int factorialOfNumber(int factorialNum) {
