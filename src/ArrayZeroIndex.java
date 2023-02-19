1305064f7e70c0e353c8942889645d1020010906
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-20 00:21:09 +0530

    ArrayZeroIndex Normal Solution

--- src/ArrayZeroIndex.java
+++ src/ArrayZeroIndex.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
-public class Practice {
+public class ArrayZeroIndex {
     public static void main(String[] args) {
-        int[] nums = {5,0,1,2,3,4};
+        int[] nums = {5, 0, 1, 2, 3, 4};
         System.out.println(Arrays.toString(arrayZeroIndex(nums)));
     }
 
@@ -13,8 +13,8 @@ public class Practice {
         }
         for (int i = 0; i < nums.length - 1; i++) {
             int temp = nums[0];
-            nums[0] = nums[i+1];
-            nums[i+1] = temp;
+            nums[0] = nums[i + 1];
+            nums[i + 1] = temp;
         }
         return nums;
     }
