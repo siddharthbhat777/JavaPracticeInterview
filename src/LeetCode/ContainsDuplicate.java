8e3895a0d83dedeaa195124f577d1ffd979843f9
Siddharth Bhat <dbgt4531@gmail.com>
2024-01-29 01:36:35 +0530

    added contains duplicate code

---
+++ src/LeetCode/ContainsDuplicate.java
@@ -0,0 +1,34 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 1, 7};
+        System.out.println("HashSet: " + hashSetContainsDuplicate(arr));
+        System.out.println("HashMap: " + hashMapContainsDuplicate(arr));
+    }
+
+    private static boolean hashSetContainsDuplicate(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (set.contains(i))
+                return true;
+            set.add(i);
+        }
+        return false;
+    }
+
+    private static boolean hashMapContainsDuplicate(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            if (map.containsKey(i)) {
+                return true;
+            }
+            map.put(i, 1);
+        }
+        return false;
+    }
+}
