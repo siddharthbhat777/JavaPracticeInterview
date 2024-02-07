e855bf1a1c0e78141452b2551a984c5f5ad25f81
Siddharth Bhat <dbgt4531@gmail.com>
2024-02-07 21:41:28 +0530

    added new program

---
+++ src/LeetCode/ReverseString.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] s = new char[]{'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+    }
+
+    private static void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < s.length/2; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+        System.out.println(s);
+    }
+}
